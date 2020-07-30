7d0769c1bee5077947e8b6849023a1c79e3af2a8
oralov-sergey <oralov-sergey@yandex.ru>
2020-07-30 22:33:10 +0400

    remarks for everything in SuperClass

--- src/main/java/SuperClass.java
+++ src/main/java/SuperClass.java
@@ -9,45 +9,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.assertEquals;
 
 public class SuperClass {
-    private static final int TIME_OUT_IN_SECONDS = 10;
-    private static WebDriver driver;
+    private static final int TIME_OUT_IN_SECONDS = 10;//created final variable
+    private static WebDriver driver;//created variable
+    private static WebDriverWait wait;//created variable
 
     @Before
     public void startWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");//path(way) to start WebDriver
+        driver = new ChromeDriver();//class instance
+        wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);//created class instance of WebdriverWait and gave it 2 parameters
     }
 
     public void waitElementAndClick(String xpath) {
-        WebDriverWait wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).click();//executed "until" gave it xpath and performed click
     }
 
     public void getBrowser(String url) {
-        driver.get(url);
-
+        driver.get(url);//executed get command with value url (opened page by known url)
     }
 
     public void waitElementAndSendKeys(String xpath, String text) {
-        WebDriverWait wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).sendKeys(text);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).sendKeys(text);//executed "until" gave it xpath and sent text
     }
 
-    public void equalsTwoString(String expected, String actual) {
-        assertEquals("Error object not equals", expected, actual);
+    public void equalsTwoString(String expected, String actual) { //for every void: it does not return
+        assertEquals("Error object not equals", expected, actual);//compare 2 strings and show error massage if strings are not similar
     }
 
-    public String waitElementAndGetText(String xpath) {
-        WebDriverWait wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
-
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).getText();
+    public String waitElementAndGetText(String xpath) { //warning! instead of "void" write type of variable. The meaning returns.
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath))).getText();//warning! Must be wrote "return"; gave it xpath, got and after returned  text.
     }
-
 
     @After
     public void quiteDriver() {
-        driver.quit();
+        driver.quit();//quit - close WebDriver and browser. (driver.close - close only browser)
     }
 }
